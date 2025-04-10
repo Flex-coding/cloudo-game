16e4ef1e64c805fcc74e9748b3bc0634fdcb63ce
Flex-coding <104089749+Flex-coding@users.noreply.github.com>
2025-04-09 21:22:26 -0300

    Create page.tsx
    Main page.

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from './components/header'
+import Hero from './components/hero'
+import Plans from './components/plans'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Plans />
+    </>
+  );
+}
